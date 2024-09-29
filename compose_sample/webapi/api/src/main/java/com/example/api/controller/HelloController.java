f61f1b1b11144512f6b27f152dc5311d0bdbfaa6
yamabook37 <yuki.yamamoto0711@gmail.com>
2024-09-29 17:45:21 +0900

    Add SpringBoot app and DB for docker-compose

---
+++ compose_sample/webapi/api/src/main/java/com/example/api/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    public String hello(
+            @RequestParam String lang
+    ) {
+        return "Hello";
+    }
+}
